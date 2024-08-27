8a4bf8bbfcb799cc370ff875d81f289570186af5
Gabriel-Dev2024 <gabrielprudencio2008@gmail.com>
2024-08-27 12:13:17 -0300

    Adicionado try/catch nos controllers e criptografia da senha

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,87 +3,111 @@ const mongoose = require('mongoose');
 
 // Rota para criação de um novo usuário
 const create = async (req, res) => {
-  // Criando variaveis que fazem a request do body
-  const { name, username, email, password, avatar, background } = req.body;
+  
+  try {
+    // Criando variaveis que fazem a request do body
+    const { name, username, email, password, avatar, background } = req.body;
 
-  // Validando se todos os campos estão preenchidos. Se não, retorna um erro 400 e uma mensagem de erro.
-  if (!name || !username || !email || !password || !avatar || !background) {
-    res.status(400).send({ message: 'Preencha todos os campos' })
+    // Validando se todos os campos estão preenchidos. Se não, retorna um erro 400 e uma mensagem de erro.
+    if (!name || !username || !email || !password || !avatar || !background) {
+      res.status(400).send({ message: 'Preencha todos os campos' })
+    }
+
+    // Verificando se o email já está cadastrado. Se sim, retorna um erro 400 e uma mensagem de erro
+    const userEmail = await userService.createService(req.body);
+
+    // Verificando se o método create retornou um novo usuário. Se não, retorna um erro 400 e uma mensagem de erro
+    if (!userEmail) {
+      return res.status(400).send({ message: 'ERRO ao criar Usuario' })
+    }
+
+    // Se todos os campos estão preenchidos, cria um novo usuário com os dados fornecidos.
+    res.status(201).send({
+      message: 'Usuario criado com sucesso',
+      user: {
+        id: userService._id,
+        name,
+        username,
+        email,
+        password,
+        avatar,
+        background,
+      },
+    });
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
+};
 
-  // Verificando se o email já está cadastrado. Se sim, retorna um erro 400 e uma mensagem de erro
-  const userEmail = await userService.createService(req.body);
+// Rota para buscar todos os usuários
+const findAll = async (req, res) => {
 
-  // Verificando se o método create retornou um novo usuário. Se não, retorna um erro 400 e uma mensagem de erro
-  if (!userEmail) {
-    return res.status(400).send({ message: 'ERRO ao criar Usuario' })
+  try {
+    // Buscando todos os usuários cadastrados.
+    const users = await userService.findAllService();
+
+    if (users.length === 0) {
+      return res.status(400).send({ message: 'Nenhum Usuario encontrado' });
+    }
+
+    res.send(users);
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
+};
 
-  // Se todos os campos estão preenchidos, cria um novo usuário com os dados fornecidos.
-  res.status(201).send({
-    message: 'Usuario criado com sucesso',
-    user: {
-      id: userService._id,
+// Rota para buscar um usuário por ID
+const findById = async (req, res) => {
+
+  try {
+    // Recebendo o ID do usuário a ser buscado.
+    const id = req.params.id;
+
+    // Buscando o usuário pelo ID. Se não encontrar, retorna um erro 400 e uma mensagem de erro.
+    const user = await userService.findByIdService(id);
+
+    res.send(user);
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+// Rota para atualizar um usuário
+const update = async (req, res) => {
+
+  try {
+    const { name, username, email, password, avatar, background } = req.body;
+
+    // Validando se todos os campos estão preenchidos. Se não, retorna um erro 400 e uma mensagem de erro.
+    if (!name && !username && !email && !password && !avatar && !background) {
+      res.status(400).send({ message: 'Preencha todos os campos' })
+    }
+
+    const id = req.params.id;
+
+    // Buscando o usuário pelo ID. Se não encontrar, retorna um erro 400 e uma mensagem de erro.
+    const user = await userService.findByIdService(id);
+
+    // Atualizando o usuário com os dados fornecidos.
+    await userService.updateService(
+      id,
       name,
       username,
       email,
       password,
       avatar,
       background,
-    },
-  });
+    );
+
+    res.send({ message: 'Usuario atualizado com sucesso', user });
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  };
 };
-
-// Rota para buscar todos os usuários
-const findAll = async (req, res) => {
-  // Buscando todos os usuários cadastrados.
-  const users = await userService.findAllService();
-
-  if (users.length === 0) {
-    return res.status(400).send({ message: 'Nenhum Usuario encontrado' });
-  }
-
-  res.send(users)
-};
-
-// Rota para buscar um usuário por ID
-const findById = async (req, res) => {
-  // Recebendo o ID do usuário a ser buscado.
-  const id = req.params.id;
-
-  // Buscando o usuário pelo ID. Se não encontrar, retorna um erro 400 e uma mensagem de erro.
-  const user = await userService.findByIdService(id);
-
-  res.send(user);
-};
-
-// Rota para atualizar um usuário
-const update = async (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body;
-
-  // Validando se todos os campos estão preenchidos. Se não, retorna um erro 400 e uma mensagem de erro.
-  if (!name && !username && !email && !password && !avatar && !background) {
-    res.status(400).send({ message: 'Preencha todos os campos' })
-  }
-
-  const id = req.params.id;
-
-  // Buscando o usuário pelo ID. Se não encontrar, retorna um erro 400 e uma mensagem de erro.
-  const user = await userService.findByIdService(id);
-  
-  // Atualizando o usuário com os dados fornecidos.
-  await userService.updateService(
-    id,
-    name,
-    username,
-    email,
-    password,
-    avatar,
-    background,
-  );
-
-  res.send({ message: 'Usuario atualizado com sucesso', user });
-}
 
 // Exportando função para ser utilizada em outros lugares da aplicação.
 module.exports = { create, findAll, findById, update };
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 // Criado uma nova instancia com opções especificas para cada objeto
@@ -19,7 +20,8 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
     avatar: {
         type: String,
@@ -29,6 +31,12 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+// Define um middleware para criptografar a senha antes de salvar no banco de dados
+UserSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 // Define o Schema e exporta o modelo User para ser usado em outros lugares
