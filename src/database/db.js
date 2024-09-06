1df26cb05d1a7430b8114b4ee27b2c9ebcaf9457
Gabriel-Dev2024 <gabrielprudencio2008@gmail.com>
2024-09-06 12:40:23 -0300

    Aumentado time out do MongoDB

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Conectando ao MongoDB Atlas
 const connectDatabase = () => {
     mongoose.connect(process.env.MONGODB_URI,
-    )
+    {serverSelectionTimeoutMS: 50000})
         // Esperando resposta do MongoDB Atlas para o mongoose conectar
         .then(() => console.log('MongoDB Atlas Connectado com sucesso'))
         .catch((err) => console.log(err));
